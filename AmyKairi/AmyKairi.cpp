d97735ed17e59da81e58e8bb88ba4622278f482e
Kaori <super_so_nic1@hotmail.com>
2021-01-04 02:27:52 -0600

    Adding code to disable morphs from Amy's dress.

--- AmyKairi/AmyKairi.cpp
+++ AmyKairi/AmyKairi.cpp
@@ -8,6 +8,10 @@ extern "C"
 {
 	__declspec(dllexport) __declspec(dllexport) void __cdecl Init(const char* path, const HelperFunctions& helperFunctions)
 	{
+		//Disable morphs
+		WriteData<3>((void*)0x486400, 0x90u);
+		WriteData<3>((void*)0x485F31, 0x90u);
+		//Replace textures
 		ReplacePVM("amy", "AmyKairi");
 	}
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
